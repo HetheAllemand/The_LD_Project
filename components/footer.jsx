b4df1d9fe401aaa889f506e9f413e0fa1be3e2e5
HetheAllemand <118325715+HetheAllemand@users.noreply.github.com>
2023-06-27 20:33:52 -0400

    final update

---
+++ components/footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+function Footer() {
+    return (
+        <div className="footer">
+            <label>Low Depot Inc.</label>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ components/product.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import './product.css'
+import QuantityPicker from './quantityPicker';
+
+
+function Product(props) {
+    const [quantity, setQuantity] = useState(1);
+
+    useEffect(function(){
+        console.log("hello, Im a product!");
+    }, []);
+
+    function onQuantityChange(qty){
+        console.log("new value" + qty);
+        setQuantity(qty);
+
+    }
+    
+    function getTotal(){
+        let total = props.data.price * quantity;
+        return total.toFixed(2);
+    }
+
+    function addProduct(){
+        console.log("Adding" + quantity + " " + props.data.title);
+    }
+
+    return (
+        <div className="product">
+            <img src={"/images/" + props.data.image} alt=''></img>
+            <h5>{props.data.title}</h5>
+
+            <div className="prices">
+            <label>$ { getTotal() }</label>
+            {/* <label>${ props.data.price.toFixed(2)}</label> */}
+            </div>
+
+
+            <QuantityPicker onChange={onQuantityChange}></QuantityPicker>
+            <button onClick={addProduct} className='btn btn-sm btn-dark'>Add To Cart</button>
+        </div>
+    );
+}
+
+export default Product;
